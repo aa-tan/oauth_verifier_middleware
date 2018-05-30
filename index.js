48c200d200b0b51cf5900f4b30cf7eb6a5b2c25f
Aaron Tan <a.tan@keio.jp>
2018-05-30 06:52:03 +0000

    Completed middleware to capture oauth verifier

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
-app.use(express.static('static'));
-app.listen(3000);
 
+
+app.get('/', (req, res) => {
+  const verifier = req.query.oauth_verifier
+  if (verifier == null){
+    res.send("No verifier detected in URL")
+  }
+  else {
+    res.send("Verifier is set to:\n" + verifier);
+  }
+})
+
+app.get('/:exception',(req,res) => {
+  res.send("Seriously? " + req.params.exception + "? Get it together.")
+})
+
+
+app.listen(8080);
 
